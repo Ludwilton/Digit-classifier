135c460bcc54f8554576034539d7cb459731c849
Ludwilton <Ludwig.carlegrund@gmail.com>
2025-03-10 18:46:38 +0100

    "scrapping vercel deployment due to file size issues"

--- api/index.py
+++
@@ -1,5 +0,0 @@
-from main import server  # main.py should define 'server = app.server'
-from werkzeug.wrappers import Request, Response
-
-def handler(request):
-    return Response.from_app(server, request.environ)
